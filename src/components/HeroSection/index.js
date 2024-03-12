749e6d016558a875847f74dae1403f3d399abbdd
abhay734 <vermaabhay734@gmail.com>
2024-03-12 22:31:57 +0530

    Created Hero Image section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Bio } from '../../data/constants';
 import styled from "styled-components";
+import HeroImg from '../../images/Hero-1.png'
 import Typewriter from 'typewriter-effect';
 
 
@@ -92,6 +93,27 @@ const HeroRightContainer = styled.div`
 
   @media (max-width: 640px) {
     margin-bottom: 30px;
+  }
+`;
+
+
+const Img = styled.img`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  max-width: 400px;
+  max-height: 400px;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.primary};
+
+  @media (max-width: 768px) {
+    max-width: 400px;
+    max-height: 400px;
+  }
+
+  @media (max-width: 640px) {
+    max-width: 280px;
+    max-height: 280px;
   }
 `;
 
@@ -216,6 +238,7 @@ const Hero = () => {
           </HeroLeftContainer>
 
           <HeroRightContainer id="Right">
+            <Img src={HeroImg} alt="hero-image" />
           </HeroRightContainer>
         </HeroInnerContainer>
       </HeroContainer>
