6fd5fd9a33650a0d98055abe9dc83fd20f551d35
abhay734 <vermaabhay734@gmail.com>
2024-03-18 00:10:36 +0530

    Added constant data

---
+++ src/data/constants.js
@@ -0,0 +1,340 @@
+export const Bio = {
+  name: "Abhay Verma",
+  roles: [
+    "Full Stack Developer",
+    "Python Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/vermaabhay734",
+  resume:
+    "https://drive.google.com/file/d/1KMVciaKQicUtf06bQRJud3Am8hv6aE02/view?pli=1",
+  linkedin: "https://www.linkedin.com/in/vermaabhay734/",
+  insta: "https://www.instagram.com/abhay_verma143/",
+  twitter: "https://leetcode.com/vermaabhay734/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+          name: "C#",
+          image: 
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/768px-C_Sharp_Logo_2023.svg.png",
+      },
+      {
+        name: "DotNet Core",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/768px-.NET_Core_Logo.svg.png",
+      },
+      {
+        name: "MVC",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/684px-Microsoft_.NET_logo.svg.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Technologies/Framework",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Django",
+        image: "https://static.djangoproject.com/img/logos/django-logo-negative.png",
+      },
+      {
+        name: "Entity Framework",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/684px-Microsoft_.NET_logo.svg.png",
+      },
+      {
+        name: "WebApi",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/684px-Microsoft_.NET_logo.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Logo_material_design.svg/768px-Logo_material_design.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Developers Tools",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Management studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/49/Applications-database.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://logowiki.net/uploads/logo/p/postman.svg",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://a0.awsstatic.com/libra-css/images/logos/aws_logo_smile_1200x630.png",
+      },
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+];
+
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png",
+    role: "Software Engineering",
+    company: "Cognizant",
+    date: "Jan 2024 - Present",
+    desc: [
+      "Collaborated with the Cloud Platform Engineering team to enhance DevOps practices.",
+      "Managing DevOps, and streamlining the process with automation." ,
+      "Implemented and maintained CI/CD pipelines using Jenkins for efficient and automated software delivery."
+    ],
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      "CI/CD",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png",
+    role: "Jr. Software Engineer",
+    company: "Cognizant",
+    date: "Nov 2022 - Dec 2023",
+    desc: [
+      "Implemented JWT authentication to create secure login and sign-in pages protecting sensitive user information.",
+      "Implemented a password reset feature that sends a one-time password to the user’s mobile phone and email address using a third-party SMS and email delivery API.",
+      "Designed and implemented various front-end components using React, Bootstrap, and JavaScript."
+    ],
+    // skills: [
+    //   "ReactJS",
+    //   "Redux",
+    //   "NodeJs",
+    //   "Material UI",
+    //   "HTML",
+    //   "CSS",
+    //   "JavaScript",
+    // ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png",
+    role: "Programmer Analyst",
+    company: "Cognizant",
+    date: "Nov 2021 - Nov 2022",
+    desc: [
+      "Completed a 6-month Full Stack Development Training integrated DotNet with React",
+      "Developed Airline Management System during Training by following Agile Methodology.",
+      ""
+  ],
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.bujhansi.ac.in/assets/img/bulogo.png",
+    school: "Bundelkhand University, Jhansi",
+    date: "Sept 2017 - Oct 2021",
+    grade: "7.5 CGPA",
+    desc: "I earned my Bachelor's degree in Computer Science and Engineering from Bundelkhand University, Jhansi, showcasing proficiency in key subjects like Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. Beyond academics, I am actively involved in hands-on projects, collaborating with a talented team of developers to further enhance my practical skills and knowledge in the field. Additionally, I have demonstrated effective team-leading and management skills by overseeing functions within my college community.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://srgms.org/img/logo.png",
+    school: "Sumeet Rahul Goel Memorial School, Agra",
+    date: "Apr 2015 - Apr 2017",
+    grade: "67%",
+    desc: "I completed Intermediate and High School education at Sumeet Rahul Goel Memorial School, Agra, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+];
+
+
+
+export const projects = [
+  {
+    id: 9,
+    title: "Trackify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "",
+    image:
+      "",
+    tags: [
+      "Docker",
+      "AWS",
+      "DuckDNS",
+      "Eslint",
+      "Husky",
+      "CI/CD",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "Podstream",
+    date: "Apr 2023 - May 2023",
+    description:
+      "",
+    image:
+      "",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Abhay Verma",
+        img: "",
+      },
+      {
+        name: "",
+        img: "",
+        linkedin: "",
+        github: "",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "",
+    date: "Oct 2022 - Present",
+    description:
+      "",
+    image:
+      "",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    category: "",
+    github: "",
+    webapp: "",
+  },
+];
