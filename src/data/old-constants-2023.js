d0c62c131af2252b7f3c16a9bbb03c1a4baabe82
Abhay Verma <vermaabhay734@gmail.com>
2025-11-09 01:32:21 +0530

    Profile data updated.

---
+++ src/data/old-constants-2023.js
@@ -0,0 +1,380 @@
+export const Bio = {
+  name: "Abhay Verma",
+  roles: [
+    "Full Stack Developer",
+    "Dotnet Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/vermaabhay734",
+  resume:
+    "https://drive.google.com/file/d/1KMVciaKQicUtf06bQRJud3Am8hv6aE02/view?pli=1",
+  linkedin: "https://www.linkedin.com/in/vermaabhay734/",
+  insta: "https://www.instagram.com/abhay_verma143/",
+  //twitter: "https://leetcode.com/vermaabhay734/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+          name: "C#",
+          image: 
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/768px-C_Sharp_Logo_2023.svg.png",
+      },
+      {
+        name: "DotNet Core",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/768px-.NET_Core_Logo.svg.png",
+      },
+      {
+        name: "MVC",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/684px-Microsoft_.NET_logo.svg.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Technologies/Framework",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Angular Js",
+        image:
+          "https://angular.io/assets/images/logos/angular/angular.svg",
+      },
+      {
+        name: "Django",
+        image: "https://static.djangoproject.com/img/logos/django-logo-negative.png",
+      },
+      {
+        name: "Entity Framework",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/684px-Microsoft_.NET_logo.svg.png",
+      },
+      {
+        name: "WebApi",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/684px-Microsoft_.NET_logo.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Logo_material_design.svg/768px-Logo_material_design.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Developers Tools",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Visual Studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Visual_Studio_Icon.svg/1024px-Visual_Studio_Icon.svg.png",
+      },
+      {
+        name: "Git",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://logowiki.net/uploads/logo/p/postman.svg",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      { name: "Heroku",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/ec/Heroku_logo.svg",
+      },  
+
+      //{
+      //  name: "AWS",
+      //  image:
+      //    "https://a0.awsstatic.com/libra-css/images/logos/aws_logo_smile_1200x630.png",
+      //},
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Management Studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/4c/SQL_Server_Logo.png",},
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+];
+
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png",
+    role: "Software Engineering",
+    company: "Cognizant",
+    date: "Jan 2024 - Present",
+    desc: [
+      "1. Working on OCCM medical project using Angular, .NET Core APIs, and Python-based OCR services.",
+      "2. Handles both UI and API development, stored procedures, and Service related tasks.",
+      "3. Developed and optimized backend services using Python and .NET Core to enhance application performance and reliability.",
+      "4. Implemented OCR functionalities to extract and process data from medical documents, improving data accuracy and efficiency.",
+      "5. Worked on enhancing security measures within the application to protect sensitive medical data.",
+      "6. Participated in Agile ceremonies, contributing to sprint planning, daily stand-ups, and retrospectives to ensure project alignment and timely delivery.",
+      "7. Utilized version control systems like Git for efficient code management and collaboration among team members.",
+      "8. Deployed applications on cloud platforms, ensuring scalability and high availability of services.",
+      "9. Implemented unit tests and integration tests to ensure code quality and reliability.",
+      "10. Optimized database queries and improved data retrieval times, enhancing overall application performance.",
+      "11. Engaged in continuous learning and professional development to stay updated with the latest industry trends and technologies.",
+      "12. Participated in client meetings to gather requirements and provide technical insights for project improvements.",
+      "13. Analyzed and resolved technical issues, ensuring minimal downtime and optimal application performance.",
+      "14. Contributed to documentation efforts, creating technical guides and user manuals for end-users and developers.",
+    ],
+    skills: [
+      "DOTNET Core",
+      "C#",
+      "Angular",
+      "Python",
+      "OCR",
+      "SQL Server",
+      "MySQL",
+      "Git",
+      "Postman",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png",
+    role: "Jr. Software Engineer",
+    company: "Cognizant",
+    date: "Nov 2022 - Dec 2023",
+    desc: [
+      "1. Implemented JWT authentication to create secure login and sign-in pages protecting sensitive user information.",
+      "2. Implemented a password reset feature that sends a one-time password to the user’s mobile phone and email address using a third-party SMS and email delivery API.",
+      "3. Designed and implemented various front-end components using React, Bootstrap, and JavaScript.",
+      "4. Developed RESTful APIs using Node.js to handle user authentication, data retrieval, and other backend functionalities.",
+      "5. Integrated third-party services and APIs to enhance application functionality and user experience.",
+      "6. Collaborated with cross-functional teams including designers, product managers, and other developers to deliver high-quality software solutions.",
+      "7. Participated in code reviews and provided constructive feedback to team members to ensure code quality and maintainability.",
+      "8. Utilized version control systems like Git for efficient code management and collaboration among team members.",
+      "9. Optimized database queries and improved data retrieval times, enhancing overall application performance.",
+      "10. Participated in client meetings to gather requirements and provide technical insights for project improvements.",
+      "11. Analyzed and resolved technical issues, ensuring minimal downtime and optimal application performance.",
+      "12. Contributed to documentation efforts, creating technical guides and user manuals for end-users and developers.",
+    ],
+    // skills: [
+    //   "ReactJS",
+    //   "Redux",
+    //   "NodeJs",
+    //   "Material UI",
+    //   "HTML",
+    //   "CSS",
+    //   "JavaScript",
+    // ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png",
+    role: "Programmer Analyst",
+    company: "Cognizant",
+    date: "Nov 2021 - Nov 2022",
+    desc: [
+      "1. Completed a 6-month Full Stack Development Training integrated DotNet with React",
+      "2. Developed Airline Management System during Training by following Agile Methodology.",
+      "3. Collaborated with cross-functional teams to define and implement new features.",
+      "4. Participated in code reviews and contributed to team knowledge sharing."
+  ],
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.bujhansi.ac.in/assets/img/bulogo.png",
+    school: "Bundelkhand University, Jhansi",
+    date: "Sept 2017 - Oct 2021",
+    grade: "7.5 CGPA",
+    desc: "I earned my Bachelor's degree in Computer Science and Engineering from Bundelkhand University, Jhansi, showcasing proficiency in key subjects like Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. Beyond academics, I am actively involved in hands-on projects, collaborating with a talented team of developers to further enhance my practical skills and knowledge in the field. Additionally, I have demonstrated effective team-leading and management skills by overseeing functions within my college community.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://srgms.org/img/logo.png",
+    school: "Sumeet Rahul Goel Memorial School, Agra",
+    date: "Apr 2015 - Apr 2017",
+    grade: "67%",
+    desc: "I completed Intermediate and High School education at Sumeet Rahul Goel Memorial School, Agra, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+];
+
+
+
+export const projects = [
+  {
+    id: 9,
+    title: "Pi_Kart",
+    date: "",
+    description:
+      "Pi-Kart Web app, powered by Python and Django, offers seamless e-commerce experience. Manage products, track orders, and handle feedback. Secure PayPal gateway for smooth transactions. Easy login, shopping, and logout for users.",
+    image:
+      "https://raw.githubusercontent.com/vermaabhay734/abhay/master/src/images/Projects/pikart.jpg",
+    tags: [
+      "Python",
+      "Django",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "BootStrap",
+      "PostgreSQL",
+      "Postman",
+      "Git"
+    ],
+    category: "website",
+    github: "https://github.com/vermaabhay734/Pi_Kart-django",
+    webapp: "",
+  },
+  {
+    id: 1,
+    title: "TextUtils",
+    date: "",
+    description:
+      "TextUtils is a React-based web app designed to help users analyze and manipulate text in various ways. The app offers a user-friendly interface and provides functionality for tasks such as converting text case, copying and clearing text, removing extra spaces, and counting words and characters. Additionally, it includes a feature to estimate the reading time for a given text. The app is versatile and can be utilized for a wide range of text analysis purposes.",
+    image:
+      "https://raw.githubusercontent.com/vermaabhay734/abhay/master/src/images/Projects/textutils.png",
+    tags: ["React Js", "HTML", "CSS", "JavaScript", "BootStrap", "Git"],
+    category: "web app",
+    github: "https://github.com/vermaabhay734/textutils",
+    webapp: "https://vermaabhay734.github.io/textutils/",
+  },
+  
+  {
+    id: 2,
+    title: "Expense Tracker",
+    date: "",
+    description:
+      "Expense Tracker is a user-friendly web application built with React, designed to simplify expense management and tracking. With its intuitive interface, users can effortlessly manage their expenses according to their specific needs. Whether it's keeping track of daily spending, categorizing expenses, or setting budgets, Expense Tracker empowers users to take control of their finances efficiently. Stay organized, monitor your spending patterns, and make informed financial decisions with Expense Tracker.",
+    image:
+      "https://raw.githubusercontent.com/vermaabhay734/abhay/master/src/images/Projects/Expense-tracker.png",
+    tags: [
+      "React Js",
+      "Node Js",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Git",
+    ],
+    category: "web app",
+    github: "https://github.com/vermaabhay734/Expense-Tracker",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "Gaming Zone",
+    date: "",
+    description:
+      "Gaming Zone is an interactive project that offers a diverse range of classic and entertaining games for users to enjoy. From nostalgic favorites like Snakes, Hangman, and Tic Tac Toe to more strategic challenges like Turtle Race and BlackJack, there's something for everyone. Whether you're testing your luck, sharpening your mind, or simply seeking a fun distraction, Gaming Zone provides an immersive gaming experience right at your fingertips. Get ready to embark on thrilling adventures and compete against friends or challenge yourself to beat your own high scores. Join us in the Gaming Zone for endless hours of entertainment and excitement!",
+    image:
+      "https://raw.githubusercontent.com/vermaabhay734/abhay/master/src/images/Projects/python-games.jpeg",
+    tags: [
+      "Python",
+      "Tkinter",
+    ],
+    category: "games",
+    github: "https://github.com/vermaabhay734/python-games",
+    webapp: "",
+  },
+  // {
+  //   id: 4,
+  //   title: "Todo Web App",
+  //   date: "",
+  //   description:
+  //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+  //   image:
+  //     "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+  //   category: "web app",
+  //   github: "",
+  //   webapp: "",
+  // },
+];
